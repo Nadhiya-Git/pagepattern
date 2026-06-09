4598b9a734c5607173ba251114d01b03caa8f73c
Nadhiya Subramani <nadhiya.s@excelacom.in>
2026-06-09 23:11:43 +0530

    changes commited for prodcutupdation

--- PageObject/Loginpage.js
+++ PageObject/Loginpage.js
@@ -6,32 +6,29 @@ this.page=page;
      this.password=page.locator('#userPassword');
     this.loginbutton=page.locator('#login');
 
-    this.product=page.locator(".card-body").filter({hastesxt:"iphone 13 pro"});
+    this.products = page.locator('.card-body');
 
     }
 
 
     async navigateToLoginPage(){
-
-await this.page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-}
-async loginvaliation(username, password) {
-    await this.username.type(username);
-    await this.password.type(password);
-    await this.loginbutton.click();
-
-  const tiles=  await this.product.textcontents();
-    console.log(tiles);
-    for(let i=0;i<titles.length;i++){
-      if (await products.nth(i).locator("b").textContent() === "ZARA COAT 3") {
-            {
-            await this.page.nth(i).locator("text='Add To Cart'").click();
-        }}
-
-        
+        await this.page.goto('https://rahulshettyacademy.com/client/#/auth/login');
     }
 
-}
+    async loginvaliation(username, password) {
+        await this.username.type(username);
+        await this.password.type(password);
+        await this.loginbutton.click();
+
+        const titles = await this.products.locator('b').allTextContents();
+        console.log(titles);
+        for (let i = 0; i < titles.length; i++) {
+            if (titles[i].trim() === 'ZARA COAT 3') {
+                await this.products.nth(i).locator("text='Add To Cart'").click();
+                break;
+            }
+        }
+    }
 
 }
 
